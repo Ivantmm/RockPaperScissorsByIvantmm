7f28c4aa84234ae3a10a79a77f5011e53a391ca3
Ivan Sabev <154736150+Ivantmm@users.noreply.github.com>
2024-10-06 15:54:28 +0300

    new

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,40 +3,51 @@ import random
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
+player_move_new_game = ""
 
-player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
+while True:
 
-if player_move == "r":
-    player_move = rock
-elif player_move == "p":
-    player_move = paper
-elif player_move == "s":
-    player_move = scissors
-else:
-    raise SystemExit("Invalid Input. Try agin...")
+    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
-computer_random_number = random.randint(1,3)
+    if player_move == "r":
+        player_move = rock
+    elif player_move == "p":
+        player_move = paper
+    elif player_move == "s":
+        player_move = scissors
+    else:
+        raise SystemExit("Invalid Input. Try agin...")
 
-computer_move = ""
+    computer_random_number = random.randint(1,3)
 
-if computer_random_number == 1:
-   computer_move = rock
-elif computer_random_number == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    computer_move = ""
 
-print(f"The computer chose {computer_move}")
+    if computer_random_number == 1:
+       computer_move = rock
+    elif computer_random_number == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == paper and computer_move == rock) or \
-        (player_move == scissors and computer_move == paper):
-    print("\033[32mYou win!\033[0m")
-elif player_move == computer_move:
-    print("\033[33mDraw!\033[0m")
-else:
-    print("\033[31mYou lose!\033[0m")
+    print(f"The computer chose {computer_move}")
 
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == paper and computer_move == rock) or \
+            (player_move == scissors and computer_move == paper):
+        print("\033[32mYou win!\033[0m")
+    elif player_move == computer_move:
+        print("\033[33mDraw!\033[0m")
+    else:
+        print("\033[31mYou lose!\033[0m")
+
+    player_move_new_game = input("Type[yes] to Play Agin or [no] to quit: ")
+
+    if player_move_new_game == "yes":
+        continue
+    else:
+        break
+
+print("Thank you for playing")
 
 
 
